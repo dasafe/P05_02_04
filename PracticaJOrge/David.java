d32a6f5d957d8bf36c9ea9fd1a4cc8be1ebc9a1c
TheFreakOut <jorgetabuenca99@gmail.com>
2019-03-20 09:03:15 +0100

    gojinmfpjod

--- PracticaJOrge/David.java
+++ PracticaJOrge/David.java
@@ -4,6 +4,9 @@ public class David {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("probando");
+		
+		System.out.println("Cambio para resolver conflicto");
+		
 	}
 
 }
